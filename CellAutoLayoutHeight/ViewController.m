c381b05bea616c80dac81955e7424ff742642b88
lichengdong1 <13521828186@163.com>
2024-04-18 11:48:55 +0800

    no message

--- CellAutoLayoutHeight/ViewController.m
+++ CellAutoLayoutHeight/ViewController.m
@@ -30,8 +30,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.array = [NSMutableArray arrayWithObjects:@"1111准备一个汉字数组，用于随机选择汉字", @"2222生成多个字符串，构成字符串数组",@"准备一个汉准备一个汉字数组，用于随机选择汉字字数组，用于随机选择汉字",nil];
-    [self configSubViews];
+    
+    [self testMasonry];
+
+    
+//    self.array = [NSMutableArray arrayWithObjects:@"1111准备一个汉字数组，用于随机选择汉字", @"2222生成多个字符串，构成字符串数组",@"准备一个汉准备一个汉字数组，用于随机选择汉字字数组，用于随机选择汉字",nil];
+//    [self configSubViews];
 }
 - (void)configSubViews {
     [self.view addSubview:self.tableView];
@@ -40,6 +44,36 @@
         make.left.right.equalTo(self.view);
         make.bottom.equalTo(self.view.mas_bottom).offset(-83);
     }];
+}
+-(void)testMasonry{
+    UIView *containerView = [[UIView alloc]init];
+    containerView.backgroundColor = UIColor.redColor;
+    [self.view addSubview:containerView];
+    
+    UILabel *label = [[UILabel alloc]init];
+    label.numberOfLines = 0;
+    label.backgroundColor = UIColor.blueColor;
+    label.text = @"大发了快递放了卡就收到了封口机阿拉山斯江东父老卡斯蒂略饭卡善良的开房记录卡上打飞机卢卡斯江东父老控件阿斯利康的房间里卡时间都浪费空间阿拉山口";
+    [containerView addSubview:label];
+    
+    
+    [containerView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view).offset(100);
+        make.right.equalTo(self.view).offset(-100);
+        make.left.equalTo(self.view).offset(100);
+    }];
+    //如果不给label设置高度的话，只给上下左右 可以自适应算出高度，从而撑开containerView
+    
+    [label mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(containerView.mas_left).offset(10);
+        make.right.equalTo(containerView.mas_right).offset(-10);
+        make.top.equalTo(containerView.mas_top).offset(10);
+        make.bottom.equalTo(containerView.mas_bottom).offset(-10);
+//        make.height.mas_equalTo(20);
+    }];
+    
+    
+    
 }
 - (UITableView *)tableView {
     if (!_tableView) {
--- Podfile
+++ Podfile
@@ -1,11 +1,17 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
+platform :ios, '12.0'
 
 target 'CellAutoLayoutHeight' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 pod 'Masonry'
-  # Pods for CellAutoLayoutHeight
+pod 'AFNetworking'
+#pod 'MBProgressHUD',:git => 'https://github.com/jdg/MBProgressHUD.git',:commit => '18c442d'
+#pod 'SVProgressHUD','2.3.1'
+#pod 'IQKeyboardManager','6.5.18'
+#pod 'SDWebImage','5.19.1'
+#pod 'SnapKit','5.7.1'
 
   target 'CellAutoLayoutHeightTests' do
     inherit! :search_paths
